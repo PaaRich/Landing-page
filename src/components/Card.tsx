d7c967dc7c44c072bd156ea2babe73f61c6169fb
PaaRich <siehrichmond50@gmail.com>
2025-03-09 09:00:06 +0000

    cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
-import { BsCart4 } from "react-icons/bs";
 
-const Card = ({ className, title, text }: { className?: string; title: string; text: string }) => {
+
+const Card = ({ className, title, text,icon }: { className?: string; title: string; text: string, icon:React.ReactNode}) => {
   return (
-      <div className={`relative w-1/4 h-full ${className}`}>
-      <Link className="hover:text-white text-[var(--primary-color)] w-full mx-auto h-full flex flex-col justify-center items-center text-center" to="#">
+      <div className={` relative w-1/4 h-full ${className}`}>
+      <Link className="group hover:text-white text-[var(--primary-color)] p-4 h-full flex flex-col justify-center items-center text-center" to="#">
                 <div>
-                  <BsCart4 size={50}/>
+                  {icon}
                 </div>
               <h1 className="text-2xl font-light mt-2 tracking-wide">{ title}</h1>
               <p className=" my-5 font-light tracking-wide">{ text}</p>
-                <p>Read more</p>
+                <p className="group-hover:underline">Read more</p>
             </Link>
       </div>
   )
--- src/components/Heroheader.tsx
+++ src/components/Heroheader.tsx
@@ -1,5 +1,9 @@
 import Button from "./Button";
 import Card from "./Card";
+import { BsCart4 } from "react-icons/bs";
+import { IoSchoolOutline } from "react-icons/io5";
+import { MdLiveTv } from "react-icons/md";
+import { GiGiftOfKnowledge } from "react-icons/gi";
 
 const Heroheader = () => {
   return (
@@ -14,12 +18,12 @@ const Heroheader = () => {
       </div>
 
       {/* cards */}
-      <div className="text-black absolute top-[85%] left-1/2 -translate-x-1/2 w-[80%]  h-[300px] bg-white shadow-xl">
+      <div className="text-black absolute top-[85%] left-1/2 -translate-x-1/2 w-[90%]  h-[300px] bg-white shadow-xl">
         <div className="flex items-center justify-between h-full w-full">
-          <Card className="card" title="E-commerce" text="We provide top-notch e-commerce solutions, expert training, and media services to help businesses and individuals grow."/>
-          <Card className="card" title="Training" text="Our goal is to empower you with the tools and skills needed for success in the digital world."/>
-          <Card className="card" title="Media" text="We provide top-notch e-commerce solutions, expert training, and media services to help businesses and individuals grow."/>
-          <Card className="card" title="Media" text="We provide top-notch e-commerce solutions, expert training, and media services to help businesses and individuals grow."/>
+          <Card icon={<BsCart4 size={50}/>} className="card" title="E-commerce" text="We provide top-notch e-commerce solutions, expert training, and media services to help businesses and individuals grow."/>
+          <Card icon={<IoSchoolOutline size={50}/>} className="card" title="Training" text="Our goal is to empower you with the tools and skills needed for success in the digital world."/>
+          <Card icon={<MdLiveTv size={50}/>} className="card" title="Media" text="We provide top-notch e-commerce solutions, expert training, and media services to help businesses and individuals grow."/>
+          <Card icon={<GiGiftOfKnowledge size={50}/> } className="card" title="Idea Bank" text="We provide top-notch e-commerce solutions, expert training, and media services to help businesses and individuals grow."/>
         </div>
       </div>
     </div>
